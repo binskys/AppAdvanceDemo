2964b2aab162b4e830f518892b5f8c1a4082da29
xiaobin <xiaobin@ruiec.cn>
2021-01-05 17:46:23 +0800

    代码备份

---
+++ app/src/main/java/com/function/imagecrop/MainActivity.java
@@ -0,0 +1,24 @@
+package com.function.imagecrop;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+/**
+ * @author Zero degree
+ * @date 2021/1/4 15:06
+ * @功能:
+ */
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/function/imagecrop/MyApp.java
@@ -0,0 +1,18 @@
+package com.function.imagecrop;
+
+import android.app.Application;
+
+
+/**
+ * @author Zero degree
+ * @date 2021/1/4 14:18
+ * @功能:
+ */
+
+public class MyApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
